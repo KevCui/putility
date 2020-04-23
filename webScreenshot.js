a2e71d43c9c99fc44034c157ca5caf1e383f356f
Kevin Cui <krazycavin@gmail.com>
2020-04-23 18:40:02 +0200

    Add webScreenshot.js

---
+++ webScreenshot.js
@@ -0,0 +1,20 @@
+// Usage:
+//   phantomjs webScreenshot <url>
+
+var system = require('system');
+var args = system.args;
+var url = args[1];
+
+if (url === undefined) {
+  console.log("[ERROR] Missing input <url>");
+  phantom.exit();
+}
+
+var page = require('webpage').create();
+var timestamp = Math.floor(Date.now() / 1000);
+page.open(url, function(status) {
+  if(status === "success") {
+    page.render(timestamp + '.png');
+  }
+  phantom.exit();
+});
