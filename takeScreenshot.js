e2b2dcaec289a7ae7121d0786333459dbbe00add
Kevin Cui <krazycavin@gmail.com>
2020-04-24 09:57:51 +0200

    Update takeScreenshot.js

--- takeScreenshot.js
+++ takeScreenshot.js
@@ -1,20 +1,42 @@
-// Usage:
-//   phantomjs webScreenshot <url>
+#!/usr/bin/env node
 
-var system = require('system');
-var args = system.args;
-var url = args[1];
+const program = require('commander');
+const puppeteer = require('puppeteer-core');
 
-if (url === undefined) {
-  console.log("[ERROR] Missing input <url>");
-  phantom.exit();
-}
+program
+  .name('./fetchCookie.js')
+  .usage('<url> [-w <seconds>] [-p <path>] [-s]')
+  .option('-w, --wait <millisecond>', 'optional, waitfor n milliseconds')
+  .option('-p, --path <binary_path>', 'optional, path to chrome/chromium binary\ndefault "/usr/bin/chromium"')
+  .option('-s, --show', 'optional, show browser\ndefault not show')
+  .arguments('<url>')
+  .action(function (url) {
+    _URL = url;
+  });
 
-var page = require('webpage').create();
-var timestamp = Math.floor(Date.now() / 1000);
-page.open(url, function(status) {
-  if (status === "success") {
-    page.render(timestamp + '.png');
+program.parse(process.argv);
+
+const cPath = (program.path === undefined) ? '/usr/bin/chromium' : program.path;
+const hMode = (program.show === undefined) ? true : false;
+const wMsec = (program.wait === undefined) ? '' : parseInt(program.wait);
+
+(async() => {
+  const browser = await puppeteer.launch({executablePath: cPath, headless: hMode});
+  const page = await browser.newPage();
+  const dAgent = await browser.userAgent();
+  const uAgent = dAgent.replace('Headless', '');
+  const tStamp = Math.floor(Date.now() / 1000);
+
+  await page.setUserAgent(uAgent);
+  await page.goto(_URL, {timeout: 30000, waitUntil: 'domcontentloaded'});
+
+  if (wMsec !== '') {
+    await page.waitFor(wMsec);
+  } else {
+    await page.waitForNavigation();
   }
-  phantom.exit();
-});
+
+  await page.screenshot({path:'./' + tStamp + '.jpg', type: 'jpeg', fullPage: true});
+
+  await browser.close();
+})();
